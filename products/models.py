c175045ff694a3d44c1109e981c8d226a73be6a4
Megan Michelle <saenzmegan@ymail.com>
2025-05-05 19:45:13 +0000

    Create model for mobile devices

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    BRAND_CHOICES = [
+        ('apple', 'Apple'),
+        ('samsung', 'Samsung'),
+    ]
+
+    GRADE_CHOICES = [
+        ('A', 'Grade A'),
+        ('B', 'Grade B'),
+    ]
+
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=20, choices=BRAND_CHOICES)
+    grade = models.CharField(max_length=1, choices=GRADE_CHOICES)
+    storage_gb = models.PositiveIntegerField(help_text="Enter storage in GB as an integer (e.g. 128)")
+    camera_megapixels = models.DecimalField(max_digits=4, decimal_places=1)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
