626110a6d3f64b5f3f7cf1fbb841b557f2c38f9d
Megan Michelle <saenzmegan@ymail.com>
2025-05-17 12:30:36 +0000

    Add correct database to settings.py file

--- staffsale_portal/settings.py
+++ staffsale_portal/settings.py
@@ -113,10 +113,10 @@ WSGI_APPLICATION = 'staffsale_portal.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"),
+        conn_max_age=600
+    )
 }
 
 # Password validation
