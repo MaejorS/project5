472c711d06000aa2d0f1ac5941b7c20689538288
Megan Michelle <saenzmegan@ymail.com>
2025-05-18 17:15:13 +0000

    Register customer model to admin page

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Order
+from .models import CustomerProfile, Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'product', 'stripe_payment_id', 'price_paid', 'created_at')
+
+@admin.register(CustomerProfile)
+class CustomerProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number', 'pickup_location')
