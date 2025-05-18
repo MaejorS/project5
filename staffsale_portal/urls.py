4630a2cd0bbd00be5fd5fa2c0c12417428267ccb
Megan Michelle <saenzmegan@ymail.com>
2025-05-18 13:31:59 +0000

    Add url pattern to checkout page

--- staffsale_portal/urls.py
+++ staffsale_portal/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    path('checkout/', include('checkout.urls')),
 ]
 
 if settings.DEBUG:
