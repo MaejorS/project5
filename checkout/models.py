8408afd51950314ee24ec0af8c520d783a2ba62b
Megan Michelle <saenzmegan@ymail.com>
2025-05-18 13:28:22 +0000

    Create model for order

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    stripe_payment_id = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}"
