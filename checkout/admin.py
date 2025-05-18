d40a9ff914292005aee01ce33dac6f328e93812e
Megan Michelle <saenzmegan@ymail.com>
2025-05-18 16:36:22 +0000

    Remove date time formate from order model

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
 from .models import Order
-from django.utils.timezone import localtime
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'product', 'stripe_payment_id', 'created_time')
-    
-    def created_time(self, obj):
-        return localtime(obj.created_at).strftime('%Y-%m-%d %H:%M:%S')
-
-    created_time.short_description = 'Created At'
+    list_display = ('id', 'user', 'product', 'stripe_payment_id', 'price_paid', 'created_at')
