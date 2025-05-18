4c06cd51f4c96310d089a41f1dfb7121f638e4e3
Megan Michelle <saenzmegan@ymail.com>
2025-05-18 13:29:09 +0000

    Add view for stripe payment

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+import stripe
+from django.conf import settings
+from django.shortcuts import redirect, get_object_or_404
+from products.models import Product
+from django.views import View
+from django.http import JsonResponse
 
-# Create your views here.
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+class CreateCheckoutSessionView(View):
+    def post(self, request, *args, **kwargs):
+        product_id = kwargs.get("pk")
+        product = get_object_or_404(Product, pk=product_id)
+
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[{
+                'price_data': {
+                    'currency': 'usd',
+                    'unit_amount': int(product.price * 100),  # price in cents
+                    'product_data': {
+                        'name': product.name,
+                    },
+                },
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url=request.build_absolute_uri('/') + '?success=true',
+            cancel_url=request.build_absolute_uri('/') + '?canceled=true',
+        )
+
+        return redirect(session.url, code=303)
