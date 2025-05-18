60c87927b99805b75ab34723c4b65a9d22da85e2
Megan Michelle <saenzmegan@ymail.com>
2025-05-18 13:31:36 +0000

    Add success or failure message to Stripe payment feature

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+def home_view(request):
+    if request.GET.get('success') == 'true':
+        messages.success(request, "Order placed successfully!")
+
+    if request.GET.get('canceled') == 'true':
+        messages.warning(request, "Payment was canceled.")
 
 def index(request):
     """A view to return the index page"""
