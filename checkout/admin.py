520861fcc611c8dd2d7045300468afa83fd49884
Megan Michelle <saenzmegan@ymail.com>
2025-05-18 15:54:37 +0000

    Create model to be stored in admin panel to store orders

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'product', 'stripe_payment_id', 'created_at')
+    search_fields = ('user__username', 'product__name', 'stripe_payment_id')
+    list_filter = ('created_at',)
