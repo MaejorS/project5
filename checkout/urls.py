aae98012961ae320fa5381961dc1451bc5016df4
Megan Michelle <saenzmegan@ymail.com>
2025-05-18 15:55:15 +0000

    Connect url pattern for OrderSuccessView

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateCheckoutSessionView
+from .views import CreateCheckoutSessionView, OrderSuccessView
 
 urlpatterns = [
     path('create-checkout-session/<int:pk>/', CreateCheckoutSessionView.as_view(), name='create_checkout_session'),
+    path('order-success/<int:pk>/', OrderSuccessView.as_view(), name='order_success'),
 ]
