6bc15457e51d96ea3c8e739a4c6acb8902ad5e28
Megan Michelle <saenzmegan@ymail.com>
2025-05-18 17:14:57 +0000

    Creat CustomerProfile Model in Checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -11,3 +11,13 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id} by {self.user.username}"
+
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True)
+    pickup_location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
