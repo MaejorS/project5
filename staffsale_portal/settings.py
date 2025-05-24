68bac8f759517574eb8cd175b3c12d03c255dcc3
Megan Michelle <saenzmegan@ymail.com>
2025-05-24 10:48:27 +0000

    Update database

--- staffsale_portal/settings.py
+++ staffsale_portal/settings.py
@@ -120,7 +120,7 @@ WSGI_APPLICATION = 'staffsale_portal.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 # Password validation
